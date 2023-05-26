9516330ea3ae3938ac8948a8dcb9145f2ba4db04
FlorianJ <jordany.florian@gmail.com>
2023-05-26 15:13:40 +0200

    modif main test

--- projet/main.py
+++ projet/main.py
@@ -8,7 +8,7 @@ from config.database import engine
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name} test modification')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
