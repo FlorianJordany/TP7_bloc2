1b651e20c0a195e6d21c47418b78676cef4ef786
Clementine <dupycournau@gmail.com>
2023-06-02 12:17:54 +0200

    modify Database

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TP7_bloc2
 Projet TP7 -Bloc 2 : Développement d'une application web et dossier technique du projet API BACKEND SWAGGER TESTS
+
+
+# Installation
+
+## Fichier .env
+
+Creez un fichier `.env` a la racine du projet suivant ce modele, en remplissant avec vos information de connexion a votre base de donnée
+````
+DATABASE_PORT='DATABASE_PORT'
+DRIVER='DATABASE_DRIVER'
+USERNAME='DATABASE_USERNAME'
+PASSWORD='DATABASE_PASSWORD'
+HOST='DATABASE_HOST'
+PORT='DATABASE_PORT'
+DATABASE_NAME='DATABASE_NAME'
+````
--- config/database.py
+++ config/database.py
@@ -7,16 +7,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Info de connexion a la base
-DATABASE_INFO = {"driver": "mysql+pymysql",
-                 "username": "root",
-                 "password": "root",
-                 "host": "localhost",
-                 "port": os.getenv('DATABASE_PORT'),
-                 "database_name": "fromagerie_com"
+DATABASE_INFO = {"driver": os.getenv('DRIVER'),
+                 "username": os.getenv('DB_USERNAME'),
+                 "password": os.getenv('PASSWORD'),
+                 "host": os.getenv('HOST'),
+                 "port": os.getenv('PORT'),
+                 "database_name": os.getenv('DATABASE_NAME')
                  }
 
+identifiants = f"{DATABASE_INFO['username']}:{DATABASE_INFO['password']}" if DATABASE_INFO["password"] != "" else DATABASE_INFO['username']
+
 # url de connexion de la base
-SQLALCHEMY_DATABASE_URL = f"{DATABASE_INFO['driver']}://{DATABASE_INFO['username']}:{DATABASE_INFO['password']}@{DATABASE_INFO['host']}:{DATABASE_INFO['port']}/{DATABASE_INFO['database_name']}"
+SQLALCHEMY_DATABASE_URL = f"{DATABASE_INFO['driver']}://{identifiants}@{DATABASE_INFO['host']}:{DATABASE_INFO['port']}/{DATABASE_INFO['database_name']}"
 
 # déclaration d'une base qui permet après de créer un modele et de mapper avec sql alchemy
 Base = declarative_base()
