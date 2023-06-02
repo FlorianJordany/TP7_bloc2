4837f17db5cd1def578a8d23d4d7927e90308aa2
Clementine <dupycournau@gmail.com>
2023-06-02 15:59:21 +0200

    add dtlcde_obj sql

---
+++ docs/sql_script/dtlcde_obj.sql
@@ -0,0 +1,6 @@
+INSERT INTO `t_dtlcode` (`id`, `codcde`, `qte`, `colis`, `commentaire`) VALUES
+(1, 34, 4, 1, 'Ballon'),
+(2, 34, 4, 2, 'Serviette'),
+(3, 20, 10, 1, 'Casquette'),
+(4, 20, 20, 2, 'briquet'),
+(5, 20, 50, 2, 'Serviette');
